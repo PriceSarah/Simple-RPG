e986b30ab13b2c9e0376804540bd1a0bcf0dfb6f
Sarah Price <s198041@students.aie.edu>
2019-08-21 14:50:52 -0500

    Fixed everything now. Monster works right

--- Simple  RPG/Game.cs
+++ Simple  RPG/Game.cs
@@ -10,24 +10,27 @@ namespace Simple__RPG
     {
         //player info
         string playerName = "";
+        int playerMaxHealth = 100;
         int playerHealth = 100;
+        int playerDamage = 10;
+        int monstersRemaining = 5;
         public void Start()
         {
             //the whole game basically
             welcome();
 
 
-            int monstersRemaining = 5;
+           
 
 
             //all the encounters
             bool alive = true;
 
-            while (alive && monstersRemaining>0)
+            while (alive && monstersRemaining > 0)
             {
-                Console.WriteLine("There are " + monstersRemaining + " Monsters left");
+                Console.WriteLine("There are " + monstersRemaining + " Monster(s) left");
                 Console.WriteLine("");
-                alive = encounter(20);
+                alive = encounter(20, 20);
                 monstersRemaining--;
             }
             Console.ReadKey();
@@ -41,52 +44,86 @@ namespace Simple__RPG
             Console.WriteLine("Welcome, " + playerName);
 
         }
-        bool encounter(int monsterDamage)
+        bool encounter(int monsterDamage, int monsterHealth)
         {
             //Monster Encounter!
-            
+            // monsterHealth = 30;
+
             Console.WriteLine("There is a monster in front of you!");
 
             Console.WriteLine("");
 
             string action = "";
-            Console.Write("Well " + playerName + " Do Something! (fight/flee) ");
-            action = Console.ReadLine();
-            Console.WriteLine("");
-            if (action == "fight" || action == "Fight")
+            bool survived = true;
+            while (playerHealth > 0 && monsterHealth > 0)
             {
-                //monster attack
-                Console.WriteLine("The monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
-                playerHealth = playerHealth - monsterDamage;
+
+                Console.Write("What do you want to do? (fight/flee) ");
+                action = Console.ReadLine();
                 Console.WriteLine("");
-                Console.WriteLine(playerName + " has " + playerHealth + " health left!");
-                if (playerHealth <= 0)
+
+
+
+                if (action == "fight" || action == "Fight")
                 {
-                    //player defeat
-                    Console.WriteLine(playerName + " was defeated!");
-                    return false;
+                  survived = fight(ref monsterDamage, ref monsterHealth);
+                    if (!survived)
+                    {
+                        return false;
+                    }
                 }
 
-                //player attack
-                Console.WriteLine(playerName + " attacks! the monster is defeated!");
 
+
+                else if (action == "flee" || action == "Flee")
+                {
+                    //escape
+                    Console.WriteLine(playerName + " got out safley");
+                    Console.WriteLine("");
+                    return true;
+
+                }
             }
-            else if (action == "flee" || action == "Flee")
-            {
-                //escape
-                Console.WriteLine(playerName + " got out safley");
 
+            return survived;
+
+        }
+
+        bool fight(ref int monsterDamage, ref int monsterHealth)
+        {
+            
+            //monster attack
+            Console.WriteLine("The monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
+            playerHealth = playerHealth - monsterDamage;
+            Console.WriteLine("");
+            Console.WriteLine(playerName + " has " + playerHealth + " health left!");
+            if (playerHealth <= 0)
+            {
+                //player defeat
+                Console.WriteLine(playerName + " was defeated!");
+                return false;
             }
 
-            //Work in progress
-            /*else if (action != "Fight" || action != "fight" || action != "flee" || action != "Flee")
+            //player attack
+            Console.WriteLine(playerName + " attacks! The monster takes " + playerDamage + " damage!");
+            monsterHealth -= playerDamage;
+            Console.WriteLine("The monster has " + monsterHealth + " health left");
+            if (monsterHealth <= 0)
             {
-                Console.WriteLine("Try again, (fight/flee)");
-                action = Console.ReadLine();
+                Console.WriteLine("The monster was defeated!");
+                
+                return true;
+            }
+            bool flee()
+            {
 
-            }*/
+                return true;
+            }
+            bool heal()
+            {
+                return true;
+            }
             return true;
-
         }
     }
 }
