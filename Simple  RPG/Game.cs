d836868d4dd51d13e92a1d324af9613c318acf60
Sarah Price <s198041@students.aie.edu>
2019-08-21 09:53:37 -0500

    Made the basis for a text based rpg game.

--- Simple  RPG/Game.cs
+++ Simple  RPG/Game.cs
@@ -10,9 +10,47 @@ namespace Simple__RPG
     {
         public void Start()
         {
-            //starts the game
-            Console.WriteLine("Welcome!");
-            Console.ReadLine();
+            //player info
+            string playerName = "";
+            int playerHealth = 100;
+            
+
+            //Welcome
+            Console.WriteLine("What is your name? ");
+            playerName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + playerName);
+
+            Console.WriteLine("");
+
+            //Monster Encounter!
+            int monsterDamage = 13;
+            Console.WriteLine("There is a monster in front of you!");
+
+            Console.WriteLine("");
+
+            string action = "";
+            Console.Write("Well " + playerName + " Do Something! (fight/flee) ");
+            action = Console.ReadLine();
+            if (action == "fight" || action == "Fight")
+            {
+                //monster attack
+                Console.WriteLine("The monster attacks! " + playerName + " takes " + monsterDamage + " damage!");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + " has " + playerHealth +" health left!");
+
+                //player attack
+                Console.WriteLine(playerName + " attacks! the monster is defeated!");
+
+            }
+            else if (action == "flee" || action == "Flee")
+            {
+                //escape
+                Console.WriteLine(playerName + " got out safley");
+
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
