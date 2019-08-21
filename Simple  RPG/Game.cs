2bf956d865b180f6cb5e197fb919868911c188c5
Sarah Price <s198041@students.aie.edu>
2019-08-21 09:15:04 -0500

    Add project files.

---
+++ Simple  RPG/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple__RPG
+{
+    class Game
+    {
+        public void Start()
+        {
+            //starts the game
+            Console.WriteLine("Welcome!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple  RPG/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple__RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates the game and holds the whole thing
+            Game game = new Game();
+            //This calls the game to start
+            game.Start();
+        }
+    }
+}
